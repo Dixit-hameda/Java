8a1a0e7742b68fb9c6be930cef198d4dd0961680
Dixit-hameda <240390107009@saffrony.ac.in>
2026-02-06 23:16:52 +0530

    240390107009

---
+++ Point.java
@@ -0,0 +1,39 @@
+public class Point {
+
+    int x;
+    int y;
+
+    public Point() {
+        x = 5;
+        y = 5;
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(Point p) {
+        this.x = p.x;
+        this.y = p.y;
+    }
+
+    public void display() {
+        System.out.println("Point coordinates: (" + x + ", " + y + ")");
+    }
+
+    public static void main(String[] args) {
+
+        Point p1 = new Point();
+        System.out.print("Using default constructor: ");
+        p1.display();
+
+        Point p2 = new Point(10, 20);
+        System.out.print("Using parameterized constructor: ");
+        p2.display();
+
+        Point p3 = new Point(p2);
+        System.out.print("Using copy constructor: ");
+        p3.display();
+    }
+}
